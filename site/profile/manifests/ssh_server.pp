8bf387a0308c6614b03adb4dba969f395208b889
dbortnichak-home <42786170+dbortnichak-home@users.noreply.github.com>
2018-08-29 15:32:47 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDT4Eb+BIv/2OdMdqotEoE0sFrNrPtlYRbbiOeqQ5ZE2V2AoYFh213yiU7GPvMCGYSMn4Ab03u3yXbHaHwgpeGVBuAeZfHzbo3nnT8qo+5x7HX7v3NWoSzfwIIJhLHdbTpwM4ajrc2M0vjPTK05vLeW1nbhXPrC2pqLp/TUyBjZOkFZMkBSy0Ay8R/40qfCm16z0wFB7N3b0/MEfeydLEbC8IGjQ/VizlqLZc0uv3S1xUmh93Eyr0VMrambRe59ycMyUUHRe66qUOZdwwS0+uk7MZ2YQ03rftZuI3fVtUzxe1654orFENBLpE/7Ezn1Im2jZVE3r/LHl5TYfue9dj+T',
   }  
 }
